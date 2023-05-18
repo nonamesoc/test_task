3520257c24821d94d5696ba268cf354e6ac7c57a
Yakov Gorbunov <avatargfa@gmail.com>
2023-05-18 20:39:46 +0800

    Add User model

---
+++ src/Model/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Model;
+
+class User {
+
+  protected int $id;
+
+  protected string $name;
+
+  protected string $email;
+
+  protected string $phone;
+
+  protected int $telegram_id;
+
+  public function getEmail(): string {
+    return $this->email;
+  }
+
+  public function getPhone(): string {
+    return $this->phone;
+  }
+
+  public function getTelegramId(): string {
+    return $this->telegram_id;
+  }
+
+}
